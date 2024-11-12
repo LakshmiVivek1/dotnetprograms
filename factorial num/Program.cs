65ea2b4787652e35de1fdc5940b9c1a4866d2d26
LakshmiVivek1 <143243686+LakshmiVivek1@users.noreply.github.com>
2024-11-12 07:50:52 +0530

    Add files via upload

---
+++ factorial num/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long fact = 1;
+            Console.Write("Enter the number to find factorial: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i <= n; i++)
+            {
+                fact *= i;
+            }
+
+            Console.WriteLine($"\nThe factorial of {n} is: {fact}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ fibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f1 = 0, f2 = 1, f3, i;
+            Console.Write("Enter the number of terms for the Fibonacci series: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("The Series is:");
+
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(f1 + " ");
+                f3 = f1 + f2;
+                f1 = f2;
+                f2 = f3;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
